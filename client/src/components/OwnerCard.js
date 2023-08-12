b326dd1fea1af83e4caa318c23ae071bcd5daba4
River Ferguson <riverferguson@gmail.com>
2023-08-11 18:33:45 -0700

    remove unneccesary imports

--- client/src/components/OwnerCard.js
+++ client/src/components/OwnerCard.js
@@ -1,22 +1,13 @@
 
-import { Link } from 'react-router-dom'
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import CardContent from '@mui/material/CardContent';import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
 const OwnerCard = ({owner}) => {
-const {first_name, last_name, bio, boats} = owner
+const {first_name, last_name, bio} = owner
 
 
   return (
